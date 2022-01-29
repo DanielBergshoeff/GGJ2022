e736f8619d3fd4fa046395c15ee2664b6a4f134a
Ilja Ham <ilja.ham@gmail.com>
2022-01-29 04:06:35 +0100

    Scene gemaakt, shaders geimporteerd en aangemaakt
    verder nog wat includes en materials erbij gezet, en settings mee gekloot, en mapjes aangemaakt

---
+++ Library Madness/Assets/Shaders/Outlines/LightCalculations.hlsl
@@ -0,0 +1,83 @@
+﻿#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten)
+{
+#if defined(SHADERGRAPH_PREVIEW)
+    Direction = half3(0.5, 0.5, 0);
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 0;
+#else
+
+    //half cascadeIndex = ComputeCascadeIndex(WorldPos);
+    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+
+    Light mainLight = GetMainLight(0);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+
+#if !defined(_MAIN_LIGHT_SHADOWS) || defined(_RECEIVE_SHADOWS_OFF)
+    ShadowAtten = 1.0h;
+#endif
+
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    float shadowStrength = GetMainLightShadowStrength();
+    ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture,
+        sampler_MainLightShadowmapTexture),
+        shadowSamplingData, shadowStrength, false);
+
+#endif
+}
+
+void AdditionalLights_float(float3 SpecColor, float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView, out float3 Diffuse, out float3 Specular)
+{
+    float3 diffuseColor = 0;
+    float3 specularColor = 0;
+
+#ifndef SHADERGRAPH_PREVIEW
+    Smoothness = exp2(10 * Smoothness + 1);
+    WorldNormal = normalize(WorldNormal);
+    WorldView = SafeNormalize(WorldView);
+    int pixelLightCount = GetAdditionalLightsCount();
+    for (int i = 0; i < pixelLightCount; ++i)
+    {
+        Light light = GetAdditionalLight(i, WorldPosition);
+        half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+        diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+
+        float distance = length(float3(light.direction));
+        float3 halfVec = SafeNormalize(float3(light.direction) + float3(WorldView));
+        half NdotH = half(saturate(dot(WorldNormal, halfVec)));
+        half modifier = pow(NdotH, Smoothness);
+        half3 specularReflection = SpecColor.rgb * modifier * attenuatedLightColor;
+        specularColor += specularReflection;
+        
+        // specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, float4(SpecColor, 0), Smoothness);
+    }
+#endif
+
+    Diffuse = diffuseColor;
+    Specular = specularColor;
+}
+
+
+void DirectSpecular_float(float3 Specular, float Smoothness, float3 Direction, float3 Color, float3 WorldNormal, float3 WorldView, out float3 Out
+)
+{
+    float3 specularColor = 0;
+#ifdef SHADERGRAPH_PREVIEW
+    Out = 0;
+#else
+    Smoothness = exp2(10 * Smoothness + 1);
+    WorldNormal = normalize(WorldNormal);
+    WorldView = SafeNormalize(WorldView);
+    specularColor += LightingSpecular(Color, Direction, WorldNormal, WorldView, float4(Specular, 0), Smoothness);
+    Out = specularColor;
+#endif
+}
+
+#endif
+
+
