12013813331d9bef1b93f254bb87058d64d1441f
DanielBergshoeff <dabergshoeff@gmail.com>
2022-01-29 16:54:44 +0100

    Created menus

---
+++ Library Madness/Assets/Scripts/Menu/InGameMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : Menu
+{
+    public GameObject Interface;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Interface.SetActive(!Interface.activeSelf);
+        }
+    }
+}
